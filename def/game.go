b09fdc0cf96ae086ecfbeba4ac7f185ead976f80
caiyunfeng <1129302995@qq.com>
2024-09-21 18:02:11 +0800

    game

--- def/game.go
+++ def/game.go
@@ -1,5 +1,14 @@
 package def
 
 const (
-	SlotsFu = 50
+	MahjongBanbisan = 1  // 半壁山麻将
+	MahjongWuhan    = 2  // 武汉麻将
+	MahjongKawuxing = 3  // 卡五星
+	MahjongGuobiao  = 4  // 国标
+	MahjongBug      = 5  // Bug麻将
+	PokerDoudizhu   = 30 // 斗地主
+	PokerGeban      = 31 // 割板
+	SlotsFu         = 50 // 五福临门
+	SlotsShanshui   = 51 // 山水
+	SlotsSemame     = 52 // 芝麻
 )
